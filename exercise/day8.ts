a1f77e18a7a0af7127ca473609497299a1e38c9a
rizkyagungnugroho <haidarax2@gmail.com>
2025-02-06 11:28:47 +0700

    day8

--- exercise/day8.ts
+++ exercise/day8.ts
@@ -1,22 +1,135 @@
 class Employee {
-    constructor(type, hoursPerDay, totalDays) {
-        this.type = type;
-        this.hoursPerDay = hoursPerDay;
-        this.totalDays = totalDays;
+    // name:string;
+    // workingHours:[];
+    constructor(public name: string, public workingHours: number) {
+      this.name = name;
+      this.workingHours = workingHours;
+    }
+  }
+  
+  class FullTimeEmployee extends Employee {
+    constructor(name: string, workingHours: number) {
+      super(name, workingHours);
+    }
+  
+    calculateSalary() {
+      let total = 0;
+      let total2 = 0;
+      let salary = 100_000;
+      let salar2 = 75_000;
+  
+      if (this.workingHours === 6) {
+        total += this.workingHours * 100_000;
+      } else if (this.workingHours > 6) {
+        total2 = salary * 6;
+        total += (this.workingHours - 6) * salar2;
+        total += total2;
+      } else {
+        return "Jam Kerja anda Kurang, Gaji Dipotong";
+      }
+      return total;
+    }
+  }
+  
+  const FullAndi = new FullTimeEmployee("budi", 6);
+  console.log(FullAndi.calculateSalary());
+  
+  class PartTimeEmployee extends Employee {
+    constructor(name: string, workingHours: number) {
+      super(name, workingHours);
+    }
+  
+    claculateSalary() {
+      let total = 0;
+      let total2 = 0;
+      let salary = 50_000;
+      let salar2 = 30_000;
+  
+      if (this.workingHours === 6) {
+        total += this.workingHours * 50_000;
+      } else if (this.workingHours > 6) {
+        total2 = salary * 6;
+        total += (this.workingHours - 6) * salar2;
+        total += total2;
+      } else {
+        return "Jam Kerja anda Kurang, Gaji Dipotong";
+      }
+      return total;
+    }
+  }
+  
+  const partAndi = new PartTimeEmployee("Andi", 8);
+  console.log(partAndi.claculateSalary());
+
+
+// 2 
+class Player {
+    name: string;
+    health: number;
+    power: number;
+
+    constructor(name: string, health: number = 100, power: number = 10) {
+        this.name = name;
+        this.health = health;
+        this.power = power;
     }
 
-    calculateSalary() {
-        let hourlyRate = (this.type === "full-time")
-            ? (this.hoursPerDay > 6 ? 75000 : 100000)
-            : (this.hoursPerDay > 6 ? 30000 : 50000);
-        
-        return hourlyRate * this.hoursPerDay * this.totalDays;
+    damage(power: number) { 
+        this.health -= power;
+        if (this.health < 0) this.health = 0;
+    }
+
+    useItem(item: { health: number; power: number }) {
+        this.health += item.health;
+        this.power += item.power;
+    }
+
+    showStatus() {
+        console.log(`${this.name} (Health => ${this.health}, Power => ${this.power})`);
     }
 }
 
-// Contoh penggunaan
-const employee1 = new Employee("full-time", 7, 10);
-console.log(`Total Salary: IDR ${employee1.calculateSalary()}`);
+class ShootingGame {
+    player1: Player;
+    player2: Player;
 
-const employee2 = new Employee("part-time", 5, 15);
-console.log(`Total Salary: IDR ${employee2.calculateSalary()}`);
+    constructor(player1: Player, player2: Player) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+     
+    getRandomItem(healthProb = 0.5, powerProb = 0.5) {
+        const health = Math.random() > healthProb ? 10 : 0;
+        const power = Math.random() > powerProb ? 10 : 0;
+        return { health, power };
+    }
+
+    start() {
+        console.log("Starting the shooting game...");
+        while (this.player1.health > 0 && this.player2.health > 0) {
+            this.player1.showStatus();
+            this.player2.showStatus();
+
+            const item1 = this.getRandomItem();
+            const item2 = this.getRandomItem();
+
+            this.player1.useItem(item1);
+            this.player2.useItem(item2);
+
+            this.player1.damage(this.player2.power);
+            this.player2.damage(this.player1.power);
+
+            console.log("After shooting:");
+            this.player1.showStatus();
+            this.player2.showStatus();
+        }
+
+        const winner = this.player1.health > 0 ? this.player1.name : this.player2.name;
+        console.log(`The winner is ${winner}!`);
+    }
+}
+
+const player1 = new Player("Player 1");
+const player2 = new Player("Player 2");
+const game = new ShootingGame(player1, player2);
+game.start();
---
+++ exercise/rev8.ts
@@ -0,0 +1,189 @@
+
+
+
+class Employee {
+    name:string;
+    gender:"male" | "female";
+    workingHours: number;
+
+    constructor(name:string, gender:"male" | "female"){
+        this.name=name;
+        this.gender=gender;
+        this.workingHours= 0;
+    }
+    formatPrice(price: number) {
+        return new Intl.NumberFormat("id-ID", {
+            style: "currency",
+            currency: "IDR",
+            maximumFractionDigits: 0,
+        }).format(price);
+    }
+}
+
+class FullTimeEmployee extends Employee {
+    hourlyRate:number;
+    extraHourlyRate: number;
+    
+    constructor(name:string, gender:"male" | "female") {
+        super(name,gender);
+        this.hourlyRate= 100_000;
+        this.extraHourlyRate= 50_000;
+    }
+
+    addWorkingHours(hours:number) {
+        this.workingHours =+ hours;
+    }
+
+    calculateSalery() {
+        if(this.workingHours <=6) {
+            return{
+                name:   this.name,
+                gender: this.gender,
+                status: "full time",
+                hours:  this.workingHours,
+                totalSalary: this.formatPrice(this.workingHours * this.hourlyRate),
+            };
+        } else {
+            const regularHours= 6;
+            const extraHours= this.workingHours - regularHours;
+
+            return{
+                name:   this.name,
+                gender: this.gender,
+                status: "full time",
+                hours:  this.workingHours,
+                totalSalaryRegular:this.formatPrice(regularHours * this.hourlyRate),
+                totalSalaryExtra: this.formatPrice(extraHours * this.extraHourlyRate),
+                total: this.formatPrice(regularHours * this.hourlyRate + extraHours * this.extraHourlyRate),
+            };
+        }
+    }
+    }
+
+
+class PartTimeEmployee extends Employee {
+    hourlyRate:number;
+    extraHourlyRate: number;
+    
+    constructor(name:string, gender:"male" | "female") {
+        super(name,gender);
+
+        this.hourlyRate= 100_000;
+        this.extraHourlyRate= 50_000;
+    }
+    addWorkingHours(hours:number) {
+        this.workingHours =+ hours;
+    }
+    calculateSalery() {
+        if(this.workingHours <=6) {
+            return{
+                name:   this.name,
+                gender: this.gender,
+                status: "full time",
+                hours:  this.workingHours,
+                totalSalary: this.formatPrice(this.workingHours * this.hourlyRate),
+            };
+        } else {
+            const regularHours= 6;
+            const extraHours= this.workingHours - regularHours;
+
+            return{
+                name:   this.name,
+                gender: this.gender,
+                status: "full time",
+                hours:  this.workingHours,
+                totalSalaryRegular:this.formatPrice(regularHours * this.hourlyRate),
+                totalSalaryExtra: this.formatPrice(extraHours * this.extraHourlyRate),
+                total: this.formatPrice(regularHours * this.hourlyRate + extraHours * this.extraHourlyRate),
+            };
+        }
+    }
+}
+
+const Employee1= new Employee ("jordi", "male");
+
+Employee1
+
+
+
+// soal 2
+
+class Player{
+    name:string;
+    health: number;
+    power: number;
+
+    constructor(name:string) {
+        this.name=name;
+        this.health=100;
+        this.power= 10;
+    }
+    damage(power:number) {
+        this.health -= power;
+    }
+
+
+    useItem(item: {health: number;  power: number}) {
+        this.health += item.health;
+        this.power =+ item.power;
+    }
+
+
+    showStatus(){
+        return `${this.name} -> health: ${this.health} | power ${this.power}`;
+    }
+}
+
+class ShootingGame {
+    player1: Player;
+    player2: Player;
+
+    constructor(player1: Player, player2:player) {
+        this.player1= player1;
+        this.player2= player2;
+    }
+    
+    getRandomItem(){
+    /* 
+    power =10 , health = 0
+    power =0, health = 10
+    power = 10, health = 10
+    power = 0, health = 0
+    */
+            const health = Math.random() < 0.5 ? 0 : 10;
+            const power = Math.random() <0.5 ? 0 : 10;
+            return {health, power};
+    }
+
+    start(){
+        while(this.player1.health > 0 && this.player2.health > 0 ){
+           console.log(this.player1.showStatus());
+           console.log(this.player2.showStatus());
+
+           //get random item
+           const item1= this.getRandomItem();
+           const item2= this.getRandomItem();
+
+           //use random item
+           this.player1.useItem(item1);
+           this.player2.useItem(item2);
+
+            // hit other player
+           this.player1.damage(this.player2.power);
+           this.player2.damage(this.player1.power);
+
+           console.log(this.player1.showStatus());
+           console.log(this.player2.showStatus());  
+        }
+        if (this.player1.health <= 0) {
+            return`${this.player2.name} WIN`;
+        } else {
+            return `${this.player1.name} WIN`;
+        }
+    }   
+}
+
+const aceng= new Player("aceng");
+const udin= new Player("udin");
+const game= new ShootingGame(aceng, udin);
+console.log(game.start());
